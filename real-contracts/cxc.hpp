2aaef12cd86df0663ed79d62331e0396ed0737d8
Douglas Butner <dougbutner@gmail.com>
2023-06-19 19:01:40 -0500

    Returned comments on hpp

--- real-contracts/cxc.hpp
+++ real-contracts/cxc.hpp
@@ -1,6 +1,8 @@
+//NOTE ups.hpp is starting point
+
 // === Cross-contract Constants === \\
 
-eosio::name DUMMY_NAMES[6] = { "void"_n, "dummy"_n, "null"_n, "none"_n}; // CHECK these are right
+eosio::name DUMMY_NAMES[5] = { "void"_n, "dummy"_n, "null"_n, "none"_n, "nobody"_n }; // CHECK these are right
 
 
-// === Declare Relevant Contract Actions === \\ //NOTE ups.hpp is starting point
+// === Declare Relevant Contract Actions === \\ 
